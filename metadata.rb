436da0a985f9fce587842971565f5d2dc3e3a5ad
Taylor Monacelli <taylor.monacelli@streambox.com>
2016-06-14 11:57:35 -0700

    Initial cookbook tmux_wrapper

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'tmux_wrapper'
+maintainer 'The Authors'
+maintainer_email 'taylor.monacelli@streambox.com'
+license 'all_rights'
+description 'Installs/Configures tmux_wrapper'
+long_description 'Installs/Configures tmux_wrapper'
+version '0.1.0'
